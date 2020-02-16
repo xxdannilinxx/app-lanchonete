49d2c7777b587d6eaa579d171b87220560634716
Danilo Machado <danilomachado@iMac-de-Danilo.local>
2020-02-16 18:21:42 -0300

    commit;

---
+++ backend/application/logs/log-2020-02-16.php
@@ -0,0 +1,199 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2020-02-16 01:19:12 --> Config Class Initialized
+INFO - 2020-02-16 01:19:12 --> Hooks Class Initialized
+DEBUG - 2020-02-16 01:19:12 --> UTF-8 Support Enabled
+INFO - 2020-02-16 01:19:12 --> Utf8 Class Initialized
+INFO - 2020-02-16 01:19:12 --> URI Class Initialized
+DEBUG - 2020-02-16 01:19:12 --> No URI present. Default controller set.
+INFO - 2020-02-16 01:19:12 --> Router Class Initialized
+INFO - 2020-02-16 01:19:12 --> Output Class Initialized
+INFO - 2020-02-16 01:19:12 --> Security Class Initialized
+DEBUG - 2020-02-16 01:19:12 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-02-16 01:19:12 --> Input Class Initialized
+INFO - 2020-02-16 01:19:12 --> Language Class Initialized
+INFO - 2020-02-16 01:19:12 --> Loader Class Initialized
+INFO - 2020-02-16 01:19:12 --> Controller Class Initialized
+INFO - 2020-02-16 01:19:12 --> File loaded: /Users/danilomachado/Projetos/sites/app-lanchonete/backend/application/views/welcome_message.php
+INFO - 2020-02-16 01:19:12 --> Final output sent to browser
+DEBUG - 2020-02-16 01:19:12 --> Total execution time: 0.0039
+INFO - 2020-02-16 01:19:24 --> Config Class Initialized
+INFO - 2020-02-16 01:19:24 --> Hooks Class Initialized
+DEBUG - 2020-02-16 01:19:24 --> UTF-8 Support Enabled
+INFO - 2020-02-16 01:19:24 --> Utf8 Class Initialized
+INFO - 2020-02-16 01:19:24 --> URI Class Initialized
+DEBUG - 2020-02-16 01:19:24 --> No URI present. Default controller set.
+INFO - 2020-02-16 01:19:24 --> Router Class Initialized
+INFO - 2020-02-16 01:19:24 --> Output Class Initialized
+INFO - 2020-02-16 01:19:24 --> Security Class Initialized
+DEBUG - 2020-02-16 01:19:24 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-02-16 01:19:24 --> Input Class Initialized
+INFO - 2020-02-16 01:19:24 --> Language Class Initialized
+INFO - 2020-02-16 01:19:24 --> Loader Class Initialized
+INFO - 2020-02-16 01:19:24 --> Controller Class Initialized
+INFO - 2020-02-16 01:19:24 --> File loaded: /Users/danilomachado/Projetos/sites/app-lanchonete/backend/application/views/welcome_message.php
+INFO - 2020-02-16 01:19:24 --> Final output sent to browser
+DEBUG - 2020-02-16 01:19:24 --> Total execution time: 0.0038
+ERROR - 2020-02-16 01:19:24 --> Severity: Notice --> Undefined variable: th /Users/danilomachado/Projetos/sites/app-lanchonete/index.php 4
+INFO - 2020-02-16 01:19:43 --> Config Class Initialized
+INFO - 2020-02-16 01:19:43 --> Hooks Class Initialized
+DEBUG - 2020-02-16 01:19:43 --> UTF-8 Support Enabled
+INFO - 2020-02-16 01:19:43 --> Utf8 Class Initialized
+INFO - 2020-02-16 01:19:43 --> URI Class Initialized
+DEBUG - 2020-02-16 01:19:43 --> No URI present. Default controller set.
+INFO - 2020-02-16 01:19:43 --> Router Class Initialized
+INFO - 2020-02-16 01:19:43 --> Output Class Initialized
+INFO - 2020-02-16 01:19:43 --> Security Class Initialized
+DEBUG - 2020-02-16 01:19:43 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-02-16 01:19:43 --> Input Class Initialized
+INFO - 2020-02-16 01:19:43 --> Language Class Initialized
+INFO - 2020-02-16 01:19:43 --> Loader Class Initialized
+INFO - 2020-02-16 01:19:43 --> Controller Class Initialized
+INFO - 2020-02-16 01:19:43 --> File loaded: /Users/danilomachado/Projetos/sites/app-lanchonete/backend/application/views/welcome_message.php
+INFO - 2020-02-16 01:19:43 --> Final output sent to browser
+DEBUG - 2020-02-16 01:19:43 --> Total execution time: 0.0042
+ERROR - 2020-02-16 01:19:43 --> Severity: Notice --> Undefined variable: th /Users/danilomachado/Projetos/sites/app-lanchonete/index.php 4
+INFO - 2020-02-16 11:53:55 --> Config Class Initialized
+INFO - 2020-02-16 11:53:55 --> Hooks Class Initialized
+DEBUG - 2020-02-16 11:53:55 --> UTF-8 Support Enabled
+INFO - 2020-02-16 11:53:55 --> Utf8 Class Initialized
+INFO - 2020-02-16 11:53:55 --> URI Class Initialized
+INFO - 2020-02-16 11:53:55 --> Router Class Initialized
+INFO - 2020-02-16 11:53:55 --> Output Class Initialized
+INFO - 2020-02-16 11:53:55 --> Security Class Initialized
+DEBUG - 2020-02-16 11:53:55 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-02-16 11:53:55 --> Input Class Initialized
+INFO - 2020-02-16 11:53:55 --> Language Class Initialized
+ERROR - 2020-02-16 11:53:55 --> 404 Page Not Found: Faviconico/index
+INFO - 2020-02-16 17:02:18 --> Config Class Initialized
+INFO - 2020-02-16 17:02:18 --> Hooks Class Initialized
+DEBUG - 2020-02-16 17:02:18 --> UTF-8 Support Enabled
+INFO - 2020-02-16 17:02:18 --> Utf8 Class Initialized
+INFO - 2020-02-16 17:02:18 --> URI Class Initialized
+INFO - 2020-02-16 17:02:18 --> Router Class Initialized
+INFO - 2020-02-16 17:02:18 --> Output Class Initialized
+INFO - 2020-02-16 17:02:18 --> Security Class Initialized
+DEBUG - 2020-02-16 17:02:18 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-02-16 17:02:18 --> Input Class Initialized
+INFO - 2020-02-16 17:02:18 --> Language Class Initialized
+ERROR - 2020-02-16 17:02:18 --> 404 Page Not Found: Assets/icon
+INFO - 2020-02-16 18:09:26 --> Config Class Initialized
+INFO - 2020-02-16 18:09:26 --> Hooks Class Initialized
+DEBUG - 2020-02-16 18:09:26 --> UTF-8 Support Enabled
+INFO - 2020-02-16 18:09:26 --> Utf8 Class Initialized
+INFO - 2020-02-16 18:09:26 --> URI Class Initialized
+INFO - 2020-02-16 18:09:26 --> Router Class Initialized
+INFO - 2020-02-16 18:09:26 --> Output Class Initialized
+INFO - 2020-02-16 18:09:26 --> Security Class Initialized
+INFO - 2020-02-16 18:09:26 --> Config Class Initialized
+INFO - 2020-02-16 18:09:26 --> Config Class Initialized
+INFO - 2020-02-16 18:09:26 --> Hooks Class Initialized
+DEBUG - 2020-02-16 18:09:26 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-02-16 18:09:26 --> Hooks Class Initialized
+DEBUG - 2020-02-16 18:09:26 --> UTF-8 Support Enabled
+INFO - 2020-02-16 18:09:26 --> Input Class Initialized
+INFO - 2020-02-16 18:09:26 --> Utf8 Class Initialized
+DEBUG - 2020-02-16 18:09:26 --> UTF-8 Support Enabled
+INFO - 2020-02-16 18:09:26 --> Language Class Initialized
+INFO - 2020-02-16 18:09:26 --> URI Class Initialized
+INFO - 2020-02-16 18:09:26 --> Utf8 Class Initialized
+ERROR - 2020-02-16 18:09:26 --> 404 Page Not Found: Polyfills-es20150a12d9fd31bcca967008js/index
+INFO - 2020-02-16 18:09:26 --> Router Class Initialized
+INFO - 2020-02-16 18:09:26 --> Output Class Initialized
+INFO - 2020-02-16 18:09:26 --> URI Class Initialized
+INFO - 2020-02-16 18:09:26 --> Security Class Initialized
+INFO - 2020-02-16 18:09:26 --> Router Class Initialized
+INFO - 2020-02-16 18:09:26 --> Output Class Initialized
+INFO - 2020-02-16 18:09:26 --> Security Class Initialized
+DEBUG - 2020-02-16 18:09:26 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2020-02-16 18:09:26 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-02-16 18:09:26 --> Config Class Initialized
+INFO - 2020-02-16 18:09:26 --> Input Class Initialized
+INFO - 2020-02-16 18:09:26 --> Hooks Class Initialized
+INFO - 2020-02-16 18:09:26 --> Input Class Initialized
+INFO - 2020-02-16 18:09:26 --> Language Class Initialized
+DEBUG - 2020-02-16 18:09:26 --> UTF-8 Support Enabled
+INFO - 2020-02-16 18:09:26 --> Language Class Initialized
+ERROR - 2020-02-16 18:09:26 --> 404 Page Not Found: Main-es201589bb68fde9d91daec5c8js/index
+INFO - 2020-02-16 18:09:26 --> Utf8 Class Initialized
+ERROR - 2020-02-16 18:09:26 --> 404 Page Not Found: Stylese923d658615777e9f424css/index
+INFO - 2020-02-16 18:09:26 --> URI Class Initialized
+INFO - 2020-02-16 18:09:26 --> Router Class Initialized
+INFO - 2020-02-16 18:09:26 --> Output Class Initialized
+INFO - 2020-02-16 18:09:26 --> Security Class Initialized
+DEBUG - 2020-02-16 18:09:26 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-02-16 18:09:26 --> Input Class Initialized
+INFO - 2020-02-16 18:09:26 --> Language Class Initialized
+ERROR - 2020-02-16 18:09:26 --> 404 Page Not Found: Runtime-es2015bdf73a6afc49b7e7a1f4js/index
+INFO - 2020-02-16 18:09:30 --> Config Class Initialized
+INFO - 2020-02-16 18:09:30 --> Hooks Class Initialized
+DEBUG - 2020-02-16 18:09:30 --> UTF-8 Support Enabled
+INFO - 2020-02-16 18:09:30 --> Utf8 Class Initialized
+INFO - 2020-02-16 18:09:30 --> URI Class Initialized
+INFO - 2020-02-16 18:09:30 --> Router Class Initialized
+INFO - 2020-02-16 18:09:30 --> Output Class Initialized
+INFO - 2020-02-16 18:09:31 --> Security Class Initialized
+DEBUG - 2020-02-16 18:09:31 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-02-16 18:09:31 --> Input Class Initialized
+INFO - 2020-02-16 18:09:31 --> Language Class Initialized
+ERROR - 2020-02-16 18:09:31 --> 404 Page Not Found: Faviconico/index
+INFO - 2020-02-16 18:09:35 --> Config Class Initialized
+INFO - 2020-02-16 18:09:35 --> Hooks Class Initialized
+DEBUG - 2020-02-16 18:09:35 --> UTF-8 Support Enabled
+INFO - 2020-02-16 18:09:35 --> Utf8 Class Initialized
+INFO - 2020-02-16 18:09:35 --> URI Class Initialized
+INFO - 2020-02-16 18:09:35 --> Router Class Initialized
+INFO - 2020-02-16 18:09:35 --> Output Class Initialized
+INFO - 2020-02-16 18:09:35 --> Security Class Initialized
+DEBUG - 2020-02-16 18:09:35 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-02-16 18:09:35 --> Input Class Initialized
+INFO - 2020-02-16 18:09:35 --> Language Class Initialized
+ERROR - 2020-02-16 18:09:35 --> 404 Page Not Found: Main-es589bb68fde9d91daec5c8js/index
+INFO - 2020-02-16 18:10:15 --> Config Class Initialized
+INFO - 2020-02-16 18:10:15 --> Hooks Class Initialized
+DEBUG - 2020-02-16 18:10:15 --> UTF-8 Support Enabled
+INFO - 2020-02-16 18:10:15 --> Utf8 Class Initialized
+INFO - 2020-02-16 18:10:15 --> URI Class Initialized
+INFO - 2020-02-16 18:10:15 --> Router Class Initialized
+INFO - 2020-02-16 18:10:15 --> Output Class Initialized
+INFO - 2020-02-16 18:10:15 --> Security Class Initialized
+DEBUG - 2020-02-16 18:10:15 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-02-16 18:10:15 --> Input Class Initialized
+INFO - 2020-02-16 18:10:15 --> Language Class Initialized
+ERROR - 2020-02-16 18:10:15 --> 404 Page Not Found: Frontend/platforms
+INFO - 2020-02-16 18:10:20 --> Config Class Initialized
+INFO - 2020-02-16 18:10:20 --> Hooks Class Initialized
+DEBUG - 2020-02-16 18:10:20 --> UTF-8 Support Enabled
+INFO - 2020-02-16 18:10:20 --> Utf8 Class Initialized
+INFO - 2020-02-16 18:10:20 --> URI Class Initialized
+INFO - 2020-02-16 18:10:20 --> Router Class Initialized
+INFO - 2020-02-16 18:10:20 --> Output Class Initialized
+INFO - 2020-02-16 18:10:20 --> Security Class Initialized
+DEBUG - 2020-02-16 18:10:20 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-02-16 18:10:20 --> Input Class Initialized
+INFO - 2020-02-16 18:10:20 --> Language Class Initialized
+ERROR - 2020-02-16 18:10:20 --> 404 Page Not Found: Frontend/platforms
+INFO - 2020-02-16 18:10:34 --> Config Class Initialized
+INFO - 2020-02-16 18:10:34 --> Hooks Class Initialized
+DEBUG - 2020-02-16 18:10:34 --> UTF-8 Support Enabled
+INFO - 2020-02-16 18:10:34 --> Utf8 Class Initialized
+INFO - 2020-02-16 18:10:34 --> URI Class Initialized
+INFO - 2020-02-16 18:10:34 --> Router Class Initialized
+INFO - 2020-02-16 18:10:34 --> Output Class Initialized
+INFO - 2020-02-16 18:10:34 --> Security Class Initialized
+DEBUG - 2020-02-16 18:10:34 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-02-16 18:10:34 --> Input Class Initialized
+INFO - 2020-02-16 18:10:34 --> Language Class Initialized
+ERROR - 2020-02-16 18:10:34 --> 404 Page Not Found: Frontend/platforms
+INFO - 2020-02-16 18:14:45 --> Config Class Initialized
+INFO - 2020-02-16 18:14:45 --> Hooks Class Initialized
+DEBUG - 2020-02-16 18:14:45 --> UTF-8 Support Enabled
+INFO - 2020-02-16 18:14:45 --> Utf8 Class Initialized
+INFO - 2020-02-16 18:14:45 --> URI Class Initialized
+INFO - 2020-02-16 18:14:45 --> Router Class Initialized
+INFO - 2020-02-16 18:14:45 --> Output Class Initialized
+INFO - 2020-02-16 18:14:45 --> Security Class Initialized
+DEBUG - 2020-02-16 18:14:45 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-02-16 18:14:45 --> Input Class Initialized
+INFO - 2020-02-16 18:14:45 --> Language Class Initialized
+ERROR - 2020-02-16 18:14:45 --> 404 Page Not Found: Frontend/screen
